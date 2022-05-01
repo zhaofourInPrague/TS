f808953594f864c80395db57b47e840b3741856d
zhaofourInPrague <m1325679285@163.com>
2022-05-01 21:32:22 +0800

    实现路由v6,哎有改版了

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,23 @@ import Todos from './component/Todos';
 import Counter from './component/Counter';
 import store from '@/store';
 import {Provider} from 'react-redux';
-
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 
 
 ReactDOM.render(
     <Provider store={store}>
-        <Counter/>
-        <hr/>
-        <Todos />
+        <Router>
+            <>
+                <ul>
+                    <li><Link to='/counter'>counter</Link></li>
+                    <li><Link to='/todos'>todos</Link></li>
+                </ul>
+                <Routes>
+                    <Route path='/counter' element={<Counter/>}/>
+                    <Route path='/todos' element={<Todos />}/>
+                </Routes>
+            </>
+        </Router>
     </Provider>,
     document.getElementById('root')
 )
